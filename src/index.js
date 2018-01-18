3f5e4bbec02882db4dacdac351634c91e30b9ddd
Cory Crowley <ccrowley96@gmail.com>
2018-01-18 15:57:02 -0500

    Added eslinting with qhacks lint rules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -12,4 +13,5 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container'),
+);

0d84f8a22234eef870ce316a4880146012aa5c20
Cory Crowley <ccrowley96@gmail.com>
2018-01-19 14:23:29 -0500

    FB login test

---
+++ src/components/FacebookLogin/FacebookLogin.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import FacebookLogin from 'react-facebook-login';
+
+class FBLogin extends React.Component {
+  responseFacebook(response) {
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <FacebookLogin
+        appId="152331278836309"
+        autoLoad={true}
+        fields="name,email,picture"
+        scorpe="public_profile,user_friends"
+        callback={this.responseFacebook}
+        icon="fa-facebook"
+      />
+    );
+  }
+}
+
+export default FBLogin;
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import RequestForm from './Request/Request';
+import FBLogin from './FacebookLogin/FacebookLogin';
 export default class App extends Component {
   render() {
     return (
       <div>
         <div>Queen&#39;s Rideshare!</div>
-        <RequestForm/>
+        <FBLogin/>
       </div>
     );
   }

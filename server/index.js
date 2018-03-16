efa8921d0e65e872b8d138fb207bdc01edd402f6
Cory <ccrowley96@gmail.com>
2018-03-16 01:09:09 -0400

    mail in progress

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const frameguard = require('frameguard');
 const cors = require('cors');
 const API = require('./api/index');
+const sgMail = require('@sendgrid/mail');
 const BUNDLE_DIR = path.join(__dirname, '../'); // Path to static files
 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
@@ -58,3 +59,14 @@ app.listen(port, () => {
     console.log(`Qshare client running on port: 8080!`);
   }
 );
+
+
+`// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+//   to: 'ccrowley96@gmail.com',
+//   from: 'dev@qshare.ca',
+//   subject: 'Sending with SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// };
+// sgMail.send(msg);`

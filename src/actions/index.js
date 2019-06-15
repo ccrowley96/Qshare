5398ba5acb16cc6200293c4b64493d5b2d43247d
Cory <ccrowley96@gmail.com>
2019-06-15 13:02:48 -0700

    attempting to fix heroku api link

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,7 +13,7 @@ export const LOGIN = 'login';
 export const JOIN_RIDE = 'join';
 export const LEAVE_RIDE = 'leave';
 
-const LOCAL_ROOT_URL = process.env.LOCAL_ROOT_URL;
+const LOCAL_ROOT_URL = process.env.LOCAL_ROOT_URL + "/api";
 
 export function fb_user_state(userInfo) {
   return {
